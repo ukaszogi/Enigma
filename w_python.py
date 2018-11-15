55f1c41390609dbfc94259ed9aa757693c7a9139
Alset-N <34602864+ukaszogi@users.noreply.github.com>
2018-11-15 21:25:04 +0100

    Update w_python.py

--- w_python.py
+++ w_python.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+wybór = [0, 0, 0]
+rotor = [
+[],
+[],
+[],
+[],
+[]
+]
+try:
+  wybór[0] = int(imput("Podaj numer 1, rotora: "))
+  wybór[1] = int(imput("Podaj numer 2, rotora: "))
+  wybór[2] = int(imput("Podaj numer 3, rotora: "))
+except ValueError as err:
+  print(err)
+  
