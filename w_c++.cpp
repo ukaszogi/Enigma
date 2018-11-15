45c1b4d941d8496afba08cab127b3ee6321b8a4f
Alset-N <34602864+ukaszogi@users.noreply.github.com>
2018-11-15 21:17:30 +0100

    Update w_c++.cpp

--- w_c++.cpp
+++ w_c++.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+char rotor[5][24]
+
 int main()
 {
+    rotor[0][0] = '13';
+    rotor[0][1] = '20';
+    rotor[0][2] = '3';
     cout << "Hello World!" << endl;
 }
